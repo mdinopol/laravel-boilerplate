242d5fcfb55a7669115f93a4dc56553e7650fcd9
mdinopol <ammd.softdev@gmail.com>
2023-08-10 21:33:38 +0800

    access token listener and revoke existing, tests, middleware fixes

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Listeners\RevokeUserExistingToken;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Laravel\Passport\Events\AccessTokenCreated;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+
+        AccessTokenCreated::class => [
+            RevokeUserExistingToken::class,
         ],
     ];
 
